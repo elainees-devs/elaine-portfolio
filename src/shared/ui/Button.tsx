ee9eec2db1e7985aa8014c992b86b2a6aa477d87
elainees-devs <elainees.devs@gmail.com>
2025-05-28 22:38:27 +0300

    style: center Button content with flex and unify padding and width classes

--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -12,10 +12,9 @@ const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="bg-primary border border-primary hover:bg-secondary text-navy font-semibold
+      className="flex justify-center items-center w-32 bg-primary border border-primary hover:bg-secondary text-navy font-semibold
     text-sm sm:text-base md:text-lg
-    py-2 px-4 sm:py-3 sm:px-6
-    rounded-full transition-colors duration-300 w-36 sm:w-auto"
+    py-2 px-4 rounded-full transition-colors duration-300 w-36 sm:w-auto"
     >
       <span className="flex items-center gap-1 sm:gap-2">
         {text} <GoArrowUpRight className="text-slateblue" />
