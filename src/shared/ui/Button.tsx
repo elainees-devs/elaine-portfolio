14d9ea3892c46938fffbd7b8766dd5383c783688
elainees-devs <elainees.devs@gmail.com>
2025-06-06 12:24:46 +0300

    style: update Button to use consistent secondary styling and remove hover effect

--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -1,4 +1,4 @@
-//src/shared/ui/Button.tsx
+// src/shared/ui/Button.tsx
 import React from "react";
 import { GoArrowUpRight } from "react-icons/go";
 
@@ -12,9 +12,9 @@ const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="flex justify-center items-center w-32 bg-primary border border-primary hover:bg-secondary text-navy font-semibold
+      className="flex justify-center items-center w-32 bg-secondary border border-secondary text-navy font-semibold
     text-sm sm:text-base md:text-lg
-    py-2 px-4 rounded-full transition-colors duration-300 w-36 sm:w-auto"
+    py-2 px-4 rounded-full transition-colors duration-300 w-36 sm:w-auto cursor-pointer"
     >
       <span className="flex items-center gap-1 sm:gap-2">
         {text} <GoArrowUpRight className="text-slateblue" />
